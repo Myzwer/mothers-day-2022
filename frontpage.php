059dcb721b4d9843e627d445fad1846eabd0e30f
Myzwer <josh@onefortyfivedesign.com>
2022-04-28 14:13:27 -0400

    :construction: Save point in case I break this trying to get desktop to work.

--- frontpage.php
+++ frontpage.php
@@ -14,12 +14,11 @@
 get_header(); ?>
 
 
-    <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(to left,
+    <div class="bg-no-repeat bg-scroll bg-cover relative " style="background: linear-gradient(to left,
   rgba(181, 122, 85, 0.90),
   rgba(181, 122, 85, 0.3)
-), url('http://mothers-day-2022.local/wp-content/uploads/2022/04/Mothers-Day-Header-Cropped.jpg') left center; background-repeat: no-repeat; background-size: cover;
- height: 80vh;">
-        <div class="content-middle text-white text-left p-5">
+), url('http://mothers-day-2022.local/wp-content/uploads/2022/04/Mothers-Day-Header-Cropped.jpg') left center; background-repeat: no-repeat; background-size: cover; height: 80vh;">
+        <div class="content-middle text-white text-left p-5 xl:w-1/2">
             <h1 class="uppercase text-white text-5xl md:text-6xl font-black">Join us for mothers day at
                 Foothills
                 Church</h1>
@@ -41,29 +40,45 @@ get_header(); ?>
         </div>
     </div>
 
-    <div class="bg-tan p-5 md:hidden">
-        <div class="col-span-12">
-            <h3 class="uppercase text-2xl font-black ">Win your mom 3 months of free flowers!</h3>
-            <p class="pb-5">Fill out the form below and tell us why your mom is the best and you can win her 2 months
-                worth of fresh flowers from a place!</p>
-            <?php if (have_posts()) : while (have_posts()) : the_post();
-                the_content();
-            endwhile;
-            else: ?>
-                <p>Sorry, no posts matched your criteria.</p>
-            <?php endif; ?>
+    <div class="bg-tan p-5 xl:hidden">
+        <div class="grid grid-cols-12">
+            <div class="col-span-12 md:col-start-2 md:col-span-7">
+                <h3 class="uppercase text-2xl font-black ">Win your mom 3 months of free flowers!</h3>
+                <p class="pb-5">Fill out the form below and tell us why your mom is the best and you can win her 2 months
+                    worth of fresh flowers from a place!</p>
+                <?php if (have_posts()) : while (have_posts()) : the_post();
+                    the_content();
+                endwhile;
+                else: ?>
+                    <p>Sorry, no posts matched your criteria.</p>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 
-    <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(to left,
-  rgba(226, 218, 199, 0.40),
-  rgba(226, 218, 199, 0.40)
-), url('http://mothers-day-2022.local/wp-content/uploads/2022/04/Mothers-Day-Background.jpg') left center; background-repeat: no-repeat; background-size: cover;
-">
+    <div class="bg-no-repeat bg-scroll bg-cover relative md:hidden" style="background: linear-gradient(to left,
+          rgba(226, 218, 199, 0.40),
+          rgba(226, 218, 199, 0.40)
+        ), url('http://mothers-day-2022.local/wp-content/uploads/2022/04/Mothers-Day-Background.jpg') left center; background-repeat: no-repeat; background-size: cover;
+        ">
         <div class="grid grid-cols-12 gap-4">
             <div class="col-span-12 p-5 my-20 text-white">
                 <h2 class="uppercase text-4xl md:text-6xl font-black">Mother's Day Photo Booth</h2>
                 <p class = "text-lg">Do you need the perfect family photo this for Mother’s Day? Sunday, May 8th we’re excited to offer a special photo booth staffed by one of our photographers so your family can take a picture with mom. Check this website the following day to download your family photo.</p>
+            </div>
+        </div>
+    </div>
+
+        <div class="bg-no-repeat bg-scroll bg-cover relative hidden md:block" style="background: linear-gradient(to left,
+          rgba(226, 218, 199, 0.90),
+          rgba(226, 218, 199, 0.90)
+        ), url('http://mothers-day-2022.local/wp-content/uploads/2022/04/Mothers-Day-Background.jpg') left center; background-repeat: no-repeat; background-size: cover;
+        ">
+            <div class="grid grid-cols-12 gap-4">
+                <div class="col-span-12 p-5 my-20 text-black">
+                    <h2 class="uppercase text-4xl md:text-6xl font-black">Mother's Day Photo Booth</h2>
+                    <p class = "text-lg">Do you need the perfect family photo this for Mother’s Day? Sunday, May 8th we’re excited to offer a special photo booth staffed by one of our photographers so your family can take a picture with mom. Check this website the following day to download your family photo.</p>
+                </div>
             </div>
         </div>
 <?php
